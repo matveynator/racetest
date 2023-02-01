c03a3727f59c864d2e6e4fbf41fc6c28489c8e4e
Matvey Gladkikh <matvey@copters.ru>
2023-02-01 20:40:40 +0300

    random racetest

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -10,7 +10,7 @@ import (
 
 type Settings struct {
 	APP_NAME, VERSION, PROXY_ADDRESS, TIME_ZONE string
-	COMPETITORS, RESULTS, LAPS int
+	RIDERS, RESULTS, LAPS int
 	XML, RANDOM bool
 	RACE_TIMEOUT_DURATION, MINIMAL_LAP_TIME_DURATION time.Duration
 }
@@ -30,7 +30,7 @@ func ParseFlags() (config Settings)  {
 	flagVersion := flag.Bool("version", false, "Output version information")
 	flag.StringVar(&config.PROXY_ADDRESS, "target", "127.0.0.1:4000", "Provide IP address and port where chicha timekeeper is running.")
 	flag.StringVar(&config.TIME_ZONE, "timezone", "UTC", "Set race timezone. Example: Europe/Paris, Africa/Dakar, UTC, https://en.wikipedia.org/wiki/List_of_tz_database_time_zones")
-	flag.IntVar(&config.COMPETITORS, "competitors", 10, "Set ammount of competitors for the race.")
+	flag.IntVar(&config.RIDERS, "riders", 10, "Set ammount of riders (sportsmen) for the race.")
 	flag.IntVar(&config.RESULTS, "results", 10, "Set ammount of results each competitor sends to timekeeper.")
 	flag.IntVar(&config.LAPS, "laps", 5, "Set ammount of laps each race holds.")
 	flag.DurationVar(&config.RACE_TIMEOUT_DURATION, "timeout", 2*time.Minute, "Set race timeout duration. After this time if nobody passes the finish line the race will be stopped. Valid time units are: 's' (second), 'm' (minute), 'h' (hour).")
--- pkg/racetest/racetest.go
+++ pkg/racetest/racetest.go
@@ -1,7 +1,7 @@
 package Racetest
 
 import (
-  "time"
+	"time"
 	"fmt"
 	"math/rand"
 	"racetest/pkg/config"
@@ -9,39 +9,101 @@ import (
 )
 
 func Run(config Config.Settings) () {
+
+	var sleeptime int = 0 
+	var totalSleeptime int = 0
+
+	averageSleepSecondsInt  := int(config.MINIMAL_LAP_TIME_DURATION.Seconds())/config.RIDERS
+	fmt.Println("averageSleepSecondsInt", averageSleepSecondsInt)
+
 	//get rider names:
-	var sleeptime int
 	competitors := riders(config)
 
-	for lap := 0; lap <= config.LAPS; lap++ {
-		//shuffle riders:
-		rand.Seed(time.Now().UnixNano())
-		rand.Shuffle(len(competitors), func(i, j int) { competitors[i], competitors[j] = competitors[j], competitors[i] })
+	raceId := 0
 
-		for index, name := range competitors {
-			for result := 1; result <= config.RESULTS; result++ {
-				fmt.Println(name)
-			}
-			 if (index + 1) != len(competitors) {
-				rand.Seed(time.Now().UnixNano())
-				sleeptime = rand.Intn(10 - 1)
-				fmt.Printf("next rider in %d seconds...  index=%d, total riders=%d\n", sleeptime, index, len(competitors))
-				time.Sleep(time.Duration(sleeptime) * time.Second)
-			}
-		}
-		if lap != config.LAPS {
+	for {
+		for lap := 0; lap <= config.LAPS; lap++ {
+			//shuffle riders:
 			rand.Seed(time.Now().UnixNano())
-			sleeptime = rand.Intn(10 - 1)
-			fmt.Printf("next lap in %d seconds... lap#=%d, total laps=%d\n", sleeptime, lap, config.LAPS)
-			time.Sleep(time.Duration(sleeptime) * time.Second)
+			rand.Shuffle(len(competitors), func(i, j int) { competitors[i], competitors[j] = competitors[j], competitors[i] })
+
+			//zero total sleep time:
+			totalSleeptime = 0
+
+			for index, name := range competitors {
+
+				//not each competitor finishes each lap (75%):
+				if RandBool75() == true {
+					for result := 1; result <= config.RESULTS; result++ {
+						//not each result registered by antenna (60%):
+						if RandBool60() == true {
+
+							//print result:
+							fmt.Println(name)
+							
+						}
+					}
+
+					//if not last rider in the lap: 
+					if (index + 1) != len(competitors) {
+
+						//get random sleeptime = 1 second:
+						rand.Seed(time.Now().UnixNano())
+						sleeptime = rand.Intn(averageSleepSecondsInt)
+
+						//count total sleeptime
+						totalSleeptime = (totalSleeptime + sleeptime)
+
+						fmt.Printf("next rider in %d seconds...  index=%d, total riders=%d\n", sleeptime, index, len(competitors))
+
+						//sleep:
+						time.Sleep(time.Duration(sleeptime) * time.Second)
+					} else {
+						//if not the last lap:
+						if lap != config.LAPS {
+							//if last rider in the lap:
+							//calculate time left for the lap (to sleep after all riders checked in):
+							if time.Duration(totalSleeptime) * time.Second < config.MINIMAL_LAP_TIME_DURATION {
+								totalSleeptimeDuration := time.Duration(totalSleeptime) * time.Second
+								nextLapSleepTime := config.MINIMAL_LAP_TIME_DURATION.Seconds() - totalSleeptimeDuration.Seconds() 
+
+								fmt.Printf("Lap #%d finished, next lap in %d seconds, total laps=%d.\n", lap, int(nextLapSleepTime), config.LAPS)
+
+								time.Sleep(time.Duration(int(nextLapSleepTime)) * time.Second)
+							} else {
+
+								fmt.Printf("Lap #%d finished, next lap in %d seconds, total laps=%d.\n", lap, 0, config.LAPS)
+
+							}
+						} else {
+							//if last lap:
+							fmt.Printf("Race #%d finished, next race in %d seconds.\n", raceId, int(config.RACE_TIMEOUT_DURATION.Seconds()))
+							time.Sleep(config.RACE_TIMEOUT_DURATION)
+							//increment raceId
+							raceId++
+						}
+					}
+				}
+			}
 		}
 	}
 }
 
 func riders(config Config.Settings) (riders []string) {
-	for rider:=1; rider <= config.COMPETITORS; rider++ {
+	for rider:=1; rider <= config.RIDERS; rider++ {
 		name := namegenerator.NewNameGenerator(time.Now().UTC().UnixNano()).Generate()
 		riders = append(riders, name)
 	}
 	return
 }
+
+func RandBool75() bool {
+	rand.Seed(time.Now().UnixNano())
+	return rand.Intn(4) != 1
+}
+
+func RandBool60() bool {
+	rand.Seed(time.Now().UnixNano())
+	return rand.Intn(3) != 1
+}
+
